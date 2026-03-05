7b72d4d89ed55c841f44668aa6d9909b8ddffb3f
LBH97 <58932537+MoonLBH@users.noreply.github.com>
2026-03-05 22:02:03 +0800

    Add Flow-GRPO training branch alongside existing RL

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .interface import MolGen_Model
+from .rl_interface import MolGen_RLModel
+from .grpo_interface import MolGen_GRPOModel
+
+__all__ = [
+    "MolGen_Model",
+    "MolGen_RLModel",
+    "MolGen_GRPOModel",
+]
